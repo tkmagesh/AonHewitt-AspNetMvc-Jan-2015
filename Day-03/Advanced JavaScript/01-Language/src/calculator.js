5a2bf062df9b66e0b2fd1c203822eeec8954ab87
tkmagesh <tkmagesh77@gmail.com>
2015-01-30 17:00:33 +0530

    Day-3 Complete

---
+++ Day-03/Advanced JavaScript/01-Language/src/calculator.js
@@ -0,0 +1,27 @@
+function add(){
+	function parseArg(n){
+		if (typeof n === "function") return parseArg(n());
+		if (n instanceof Array) return add.apply(this, n);
+		return isNaN(n) ? 0 : parseInt(n,10);
+	}
+	return arguments.length <= 1 ? parseArg(arguments[0]) : parseArg(arguments[0]) + add([].slice.call(arguments,1));
+}
+
+/*
+
+arguments -> array like object supports indexer [0] [1] & length 
+this
+
+Function invocation (6)
+As a function  -> this => window
+As a method of obj -> this => obj
+Using "call" method
+Using "apply" method
+*/
+/*
+sum(10,20,30,40)
+	10 + sum(20,30,40)
+			20 + sum(30,40)
+					30 + sum(40)
+							40
+							*/
